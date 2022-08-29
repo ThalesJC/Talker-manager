03147e18256419e9d835c9fed24142fa71f3f98e
ThalesJC <thales.soul96@gmail.com>
2022-08-29 17:33:28 -0300

    feat:requisito 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 const app = require('./app');
-const talkers = require('./talker.json');
+const { getAllTalkers } = require('./services');
 
 app.use(bodyParser.json());
 
@@ -9,13 +9,26 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send('Voce esta na rota raiz');
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
 
-app.get('/talker', (_request, response) => (
-  talkers.length > 0 ? response.status(HTTP_OK_STATUS).json(talkers) : []
-));
+app.get('/talker', async (_request, response) => {
+    const talkers = await getAllTalkers();
+    return talkers ? response.status(HTTP_OK_STATUS).json(talkers) : [];
+});
+
+app.get('/talker/:id', async (request, response) => {
+  const talkers = await getAllTalkers();
+  const id = Number(request.params.id); 
+  const talker = talkers.find((e) => e.id === id);
+  console.log(talker);
+  response.status(HTTP_OK_STATUS).json(talker);
+});
+
+// app.post('/login', () => {
+
+// });
